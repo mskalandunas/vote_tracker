2ecfeba0f3d0fe3849c4ab4f47546a6a94be0212
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-14 21:36:42 -0700

    add cat objects to chart- no image

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Cat obj array
 var catArr = [];
-// Cat votes array
+// Cat total votes array
 var catTotal = [];
 
 // Cat constructor
-var cat = function(pic, total) {
+var Cat = function(pic, total) {
   this.pic = pic;
   this.total = total;
 
@@ -13,20 +13,20 @@ var cat = function(pic, total) {
 }
 
 // Creates cat objects
-var cat01 = new cat('img/01.jpg', 0);
-var cat02 = new cat('img/02.jpg', 0);
-var cat03 = new cat('img/03.jpg', 0);
-var cat04 = new cat('img/04.jpg', 0);
-var cat05 = new cat('img/05.jpg', 0);
-var cat06 = new cat('img/06.jpg', 0);
-var cat07 = new cat('img/07.jpg', 0);
-var cat08 = new cat('img/08.jpg', 0);
-var cat09 = new cat('img/09.jpg', 0);
-var cat10 = new cat('img/10.jpg', 0);
-var cat11 = new cat('img/11.jpg', 0);
-var cat12 = new cat('img/12.jpg', 0);
-var cat13 = new cat('img/13.jpg', 0);
-var cat14 = new cat('img/14.jpg', 0);
+var Cat01 = new Cat('img/01.jpg', 1); //test values
+var Cat02 = new Cat('img/02.jpg', 2);
+var Cat03 = new Cat('img/03.jpg', 3);
+var Cat04 = new Cat('img/04.jpg', 4);
+var Cat05 = new Cat('img/05.jpg', 5);
+var Cat06 = new Cat('img/06.jpg', 6);
+var Cat07 = new Cat('img/07.jpg', 7);
+var Cat08 = new Cat('img/08.jpg', 8);
+var Cat09 = new Cat('img/09.jpg', 9);
+var Cat10 = new Cat('img/10.jpg', 10);
+var Cat11 = new Cat('img/11.jpg', 11);
+var Cat12 = new Cat('img/12.jpg', 12);
+var Cat13 = new Cat('img/13.jpg', 13);
+var Cat14 = new Cat('img/14.jpg', 14);
 
 // Shuffles order of cat pic array (Knuth shuffle)
 function shuffle(array) {
@@ -59,83 +59,84 @@ function catPic2() {
   document.getElementById('pic-container-2').appendChild(img);
 }
 
-catPic1()
-catPic2()
+catPic1();
+catPic2();
+
+console.log(Cat01);
+console.log(Cat01.total);
+console.log(Cat02.total);
+console.log(catTotal);
 
 // Add vote
 // var addVote = function() {
 //   document.getElementById('pic-container-1').innerHTML = 'TEST TEXT';
 // }
 
+// Polar area chart
+var polarData = [
+  {
+    value: Cat01.total,
+    color: "#878BB6"
+  },
+  {
+    value: Cat02.total,
+    color: "#4ACAB4"
+  },
+  {
+    value: Cat03.total,
+    color: "#8ce8cc"
+  },
+  {
+    value: Cat04.total,
+    color: "#8DFFFF"
+  },
+  {
+    value: Cat05.total,
+    color: "#FF66A3"
+  },
+  {
+    value: Cat06.total,
+    color: "#66FF99"
+  },
+  {
+    value: Cat07.total,
+    color: "#85A3FF"
+  },
+  {
+    value: Cat08.total,
+    color: "#DBDB4D"
+  },
+  {
+    value: Cat09.total,
+    color: "#66FFCC"
+  },
+  {
+    value: Cat10.total,
+    color: "#FF99CC"
+  },
+  {
+    value: Cat11.total,
+    color: "#FF3333"
+  },
+  {
+    value: Cat12.total,
+    color: "#9999FF"
+  },
+  {
+    value: Cat13.total,
+    color: "#FF6600"
+  },
+  {
+    value: Cat14.total,
+    color: "#FFCC99"
+  }
+];
 
+var polarOptions = {
+  segmentShowStroke : false,
+  animateScale : true
+}
 
-// //Vote Listener
-
-// //Pie chart shell
-// var polarData = [
-//   {
-//     value: 6,
-//     color: "#878BB6"
-//   },
-//   {
-//     value: 14,
-//     color: "#4ACAB4"
-//   },
-//   {
-//     value: 15,
-//     color: "#8ce8cc"
-//   },
-//   {
-//     value: 4,
-//     color: "#8DFFFF"
-//   },
-//   {
-//     value: 11,
-//     color: "#FF66A3"
-//   },
-//   {
-//     value: 5,
-//     color: "#66FF99"
-//   },
-//   {
-//     value: 6,
-//     color: "#85A3FF"
-//   },
-//   {
-//     value: 2,
-//     color: "#DBDB4D"
-//   },
-//   {
-//     value: 11,
-//     color: "#66FFCC"
-//   },
-//   {
-//     value: 9,
-//     color: "#FF99CC"
-//   },
-//   {
-//     value: 8,
-//     color: "#FF3333"
-//   },
-//   {
-//     value: 4,
-//     color: "#9999FF"
-//   },
-//   {
-//     value: 13,
-//     color: "#FF6600"
-//   },
-//   {
-//     value: 6,
-//     color: "#FFCC99"
-//   }
-// ];
-
-// var polarOptions = {
-//   segmentShowStroke : false,
-//   animateScale : true
-// }
-
-// var favoriteCat = document.getElementById("favoriteCat").getContext("2d");
-// new Chart(favoriteCat).PolarArea(polarData, polarOptions);
-// //Pie end
+var favoriteCat = document.getElementById("favoriteCat").getContext("2d");
+new Chart(favoriteCat).PolarArea(polarData, polarOptions);
+//polar end
