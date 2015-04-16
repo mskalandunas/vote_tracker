1ad78c21ff7d1d861243dd99adbf6c536b489a39
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-16 13:51:49 -0700

    got server.js to work with local3k

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//stick express into variable.  anytime using something from another file, use require statement
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/js/'));
+app.use(express.static(__dirname + '/img/'));
+app.use(express.static(__dirname + '/deps/'));
+
+app.get('/', function(request, response) {
+  response.status(200).send('<h1>Hello Stranger!</h1>');
+});
+
+app.get('/img', function(request, response) {
+  response.send('<h1>Hello ' + request.params.person + '<h1>');
+});
+
+app.get('/*', function(request, response) {
+  response.status(404).send('ERROR 404 NOT FOUND');
+});
+
+app.get('/secret', function(request, response) {
+  response.status(200).send('OHH YOU FOUND ME');
+})
+
+app.listen(3000, function() {
+  console.log('server running');
+});
