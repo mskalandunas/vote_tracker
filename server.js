a58c1926539af6421c9be21cf4af7b51bacd0eca
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-27 21:32:30 -0700

    add public

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/deps/'));
+app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public/deps/'));
 
 app.get('/secret', function(request, response) {
   response.status(200).send('OHH YOU FOUND ME');
 })
 
 app.get('/*', function(request, response) {
-  response.status(404).sendFile(__dirname + '/404.html');
+  response.status(404).sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(app.get('port'), function() {
