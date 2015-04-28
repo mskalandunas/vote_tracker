8727a0a10de05c24d74db8a80ab4ddf9829d0f29
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-27 23:54:49 -0700

    add DS_Store to .gitignore and remove line from server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public/'));
-app.use(express.static(__dirname + '/public/deps/'));
 
 app.get('/secret', function(request, response) {
   response.status(200).send('OHH YOU FOUND ME');
